c104908e2cf9e94982636cd7b79ac7cd733cbaf7
mousta <moustaayman03@gmail.com>
2024-01-18 12:47:57 +0100

    render first person

--- 03-reviews/src/App.jsx
+++ 03-reviews/src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import people from "./data";
+import { FaQuoteRight } from "react-icons/fa";
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  console.log(people);
+  const [index, setIndex] = useState(0);
+  const { image, job, name, text } = people[index];
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">{<FaQuoteRight />}</span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </article>
+    </main>
+  );
 };
 export default App;
