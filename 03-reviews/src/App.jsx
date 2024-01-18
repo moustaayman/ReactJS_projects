a9cb3d62d6c5ace2dbeaf70f8e79774c8fb7e07f
mousta <moustaayman03@gmail.com>
2024-01-18 13:55:15 +0100

    cycling between review using the modulus "%" operator

--- 03-reviews/src/App.jsx
+++ 03-reviews/src/App.jsx
@@ -5,26 +5,16 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { image, job, name, text } = people[index];
 
-  const checkNumber = (number) => {
-    if (number > people.length - 1) {
-      return 0;
-    }
-    if (number < 0) {
-      return people.length - 1;
-    }
-    return number;
-  };
-
   const prevPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = currentIndex - 1;
-      return checkNumber(newIndex);
+      const newValue = (currentIndex - 1 + people.length) % people.length;
+      return newValue;
     });
   };
   const nextPerson = () => {
     setIndex((currentIndex) => {
-      const newIndex = currentIndex + 1;
-      return checkNumber(newIndex);
+      const newValue = (currentIndex + 1) % people.length;
+      return newValue;
     });
   };
 
@@ -33,7 +23,8 @@ const App = () => {
     if (randomNumber === index) {
       randomNumber = index + 1;
     }
-    setIndex(checkNumber(randomNumber));
+    const newIndex = randomNumber % people.length;
+    setIndex(newIndex);
   };
   return (
     <main>
