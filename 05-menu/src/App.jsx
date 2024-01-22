354db3b8161cd8107534f443c82796618cb44aa6
mousta <moustaayman03@gmail.com>
2024-01-22 09:31:48 +0100

    set an array of uninque menu items categories

--- 05-menu/src/App.jsx
+++ 05-menu/src/App.jsx
@@ -3,8 +3,17 @@ import Title from "./Title";
 import Menu from "./Menu";
 import data from "./data";
 
+const allUniqueCategories = [
+  "all",
+  ...new Set(data.map((item) => item.category)),
+];
+
 const App = () => {
   const [menu, setMenu] = useState([data]);
+  const [categories, setCategories] = useState(allUniqueCategories);
+  //build an array of all unique categories
+
+  console.log(allUniqueCategories);
   return (
     <main>
       <div className="menu">
