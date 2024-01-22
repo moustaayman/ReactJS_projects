b5faec8e0f4c3c19814aa061028db04e819dc5ce
mousta <moustaayman03@gmail.com>
2024-01-22 08:22:33 +0100

    Title component created

---
+++ 05-menu/src/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ title }) => {
+  return (
+    <div className="title">
+      <h2>{title || "Default Title"}</h2>
+      <div className="title-underline"></div>
+    </div>
+  );
+};
+export default Title;
