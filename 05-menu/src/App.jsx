88188c87457c5df24a1e710a8609d0d50686e24d
mousta <moustaayman03@gmail.com>
2024-01-22 08:29:25 +0100

    Menu component rendered

--- 05-menu/src/App.jsx
+++ 05-menu/src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import Title from "./Title";
+import Menu from "./Menu";
+import data from "./data";
 
 const App = () => {
+  const [menu, setMenu] = useState([data]);
   return (
     <main>
       <div className="menu">
-        <Title />
+        <Title title="our menu" />
+        <Menu menu={menu} />
       </div>
     </main>
   );
