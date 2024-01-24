02a22e70c8049fe2a4836e60c3ff52460defeff7
mousta <moustaayman03@gmail.com>
2024-01-24 23:39:08 +0100

    duties rendered

--- 06-tabs/src/Duties.jsx
+++ 06-tabs/src/Duties.jsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 const Duties = ({ duties }) => {
   return (
     <div>
-      {duties.map((duty, index) => {
+      {duties.map((duty) => {
         const id = uuidv4();
         return (
           <div key={id} className="job-desc">
