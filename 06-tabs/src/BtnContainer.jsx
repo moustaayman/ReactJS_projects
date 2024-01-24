ed1b356be101755a13f804fb6d6e616e2bc77473
mousta <moustaayman03@gmail.com>
2024-01-24 23:38:20 +0100

    switching between companies when clicking on the button

--- 06-tabs/src/BtnContainer.jsx
+++ 06-tabs/src/BtnContainer.jsx
@@ -1,12 +1,12 @@
 const BtnContainer = ({ jobs, index, setIndex }) => {
   return (
     <div className="btn-container">
-      {jobs.map((item, i) => {
+      {jobs.map((item, ind) => {
         return (
           <button
             key={item.id}
-            onClick={() => setIndex(i)}
-            className={i === index ? "job-btn active-btn" : "job-btn"}
+            onClick={() => setIndex(ind)}
+            className={ind === index ? "job-btn active-btn" : "job-btn"}
           >
             {item.company}
           </button>
