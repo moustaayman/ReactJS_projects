fcc49dff27bd66b6a79dfe93814b62eb270ccf42
mousta <moustaayman03@gmail.com>
2024-01-31 22:15:15 +0100

    SingleItem component implemented with the checkbox and removability

--- 10-grocery-bud/src/SingleItem.jsx
+++ 10-grocery-bud/src/SingleItem.jsx
@@ -1,4 +1,31 @@
-const SingleItem = () => {
-  return <div>SingleItem</div>;
+import { useState } from "react";
+
+const SingleItem = ({ item, editItem, removeItem }) => {
+  console.log(item);
+  const [isChecked, setIsChecked] = useState(item.completed);
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => editItem(item.id)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: isChecked && "line-through",
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => removeItem(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  );
 };
 export default SingleItem;
