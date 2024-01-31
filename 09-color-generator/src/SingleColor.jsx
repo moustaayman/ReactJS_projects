a20ddd4b5e3fbdf3de5c7ccdd235351d8279c2b2
mousta <moustaayman03@gmail.com>
2024-01-31 18:31:16 +0100

    changing the background color depending on the current color shade

--- 09-color-generator/src/SingleColor.jsx
+++ 09-color-generator/src/SingleColor.jsx
@@ -1,7 +1,10 @@
-const SingleColor = ({ color }) => {
+const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
   return (
-    <article style={{ background: `#${hex}` }}>
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+    >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
     </article>
