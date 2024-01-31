192c76628a302441f61aa3135bf420ec104718f3
mousta <moustaayman03@gmail.com>
2024-01-31 18:30:22 +0100

    adding the color when submitting the form

--- 09-color-generator/src/Form.jsx
+++ 09-color-generator/src/Form.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
-const Form = () => {
+const Form = ({ addColor }) => {
   const [color, setColor] = useState("");
   return (
     <section className="container">
       <h4>color generator</h4>
-      <form className="color-form">
+      <form
+        className="color-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addColor(color);
+        }}
+      >
         <input
           type="color"
           value={color}
