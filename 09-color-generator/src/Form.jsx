7b0086decce440376c912ed897efe880ca2e61cd
mousta <moustaayman03@gmail.com>
2024-01-31 16:27:51 +0100

    color form set

--- 09-color-generator/src/Form.jsx
+++ 09-color-generator/src/Form.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 const Form = () => {
-  return <div>Form</div>;
+  const [color, setColor] = useState("");
+  return (
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form">
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+      </form>
+    </section>
+  );
 };
 export default Form;
