cf54e91606b0843b62e985a656f40ebd1c7f45ec
mousta <moustaayman03@gmail.com>
2024-01-31 22:30:20 +0100

    SingleItem component is rended

--- 10-grocery-bud/src/Items.jsx
+++ 10-grocery-bud/src/Items.jsx
@@ -1,11 +1,16 @@
 import SingleItem from "./SingleItem";
 import { nanoid } from "nanoid";
 
-const Items = ({ items }) => {
+const Items = ({ items, editItem, removeItem }) => {
   return (
     <>
       {items.map((item) => (
-        <SingleItem key={nanoid()} item={item} />
+        <SingleItem
+          key={nanoid()}
+          item={item}
+          editItem={editItem}
+          removeItem={removeItem}
+        />
       ))}
     </>
   );
