c8b20f452176881a7faf408015e7dbcf75156e4c
mousta <moustaayman03@gmail.com>
2024-01-31 22:38:38 +0100

    setting a default list from the local storage

--- 10-grocery-bud/src/App.jsx
+++ 10-grocery-bud/src/App.jsx
@@ -7,18 +7,11 @@ import Items from "./Items";
 const setLocalStorage = (items) => {
   localStorage.setItem("list", JSON.stringify(items));
 };
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list");
-  if (list) {
-    list = JSON.parse(localStorage.getItem("list"));
-  } else {
-    list = [];
-  }
-  return list;
-};
+
+const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
