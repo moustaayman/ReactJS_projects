ba09fd60f76dd288542a03b996e4664f18c9134f
mousta <moustaayman03@gmail.com>
2024-01-31 18:34:55 +0100

    save to clipboard functionnality added

--- 09-color-generator/src/SingleColor.jsx
+++ 09-color-generator/src/SingleColor.jsx
@@ -1,9 +1,24 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({ color, index }) => {
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color copied to clipboard");
+      } catch (error) {
+        toast.error("Failed to copy color to clipboard");
+      }
+    } else {
+      toast.error("Clipboard access not available");
+    }
+  };
   const { hex, weight } = color;
   return (
     <article
       className={index > 10 ? "color color-light" : "color"}
       style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
