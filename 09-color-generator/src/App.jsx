6c2c2adc7ba072e1d8afaab721a784a2f5d4b1b8
mousta <moustaayman03@gmail.com>
2024-01-31 16:17:19 +0100

    render three components

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -1,4 +1,12 @@
+import ColorList from "./ColorList";
+import Form from "./Form";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+  return (
+    <>
+      <Form />
+      <ColorList />
+    </>
+  );
 };
 export default App;
