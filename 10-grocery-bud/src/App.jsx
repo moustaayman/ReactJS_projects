f22e1876f0867ce5ed209c0d7ef27569b10e4873
mousta <moustaayman03@gmail.com>
2024-01-31 21:47:50 +0100

    Form component rendered & the logic of adding an item is implemented

--- 10-grocery-bud/src/App.jsx
+++ 10-grocery-bud/src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import Form from "./Form";
+import { nanoid } from "nanoid";
+import { ToastContainer, toast } from "react-toastify";
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+  const [items, setItems] = useState([]);
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    toast.success("item added to the list");
+  };
+  return (
+    <section className="section-center">
+      <Form addItem={addItem} />
+    </section>
+  );
 };
 
 export default App;
