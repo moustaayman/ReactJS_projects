f31a57b549ba9576b7333dc49fd6b254e18961b3
mousta <moustaayman03@gmail.com>
2024-01-31 17:38:29 +0100

    colors array generated using values.js & passed to the ColorList component

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
+import Values from "values.js";
 
 const App = () => {
+  const [colors, setColors] = useState([new Values("#f15025").all(10)]);
   return (
     <>
       <Form />
-      <ColorList />
+      <ColorList ColorList={colors} />
     </>
   );
 };
