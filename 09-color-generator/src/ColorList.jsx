f81131fd75d8dc180c3ec725d30b1e7327638f3b
mousta <moustaayman03@gmail.com>
2024-01-31 18:29:50 +0100

    index property rendered as well

--- 09-color-generator/src/ColorList.jsx
+++ 09-color-generator/src/ColorList.jsx
@@ -4,8 +4,8 @@ import SingleColor from "./SingleColor";
 const ColorList = ({ colors }) => {
   return (
     <section className="colors">
-      {colors.map((color) => {
-        return <SingleColor key={nanoid()} color={color} />;
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
       })}
     </section>
   );
