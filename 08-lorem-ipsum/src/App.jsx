3d2a62b31fac2abc45be03fe2e8caa21da696420
mousta <moustaayman03@gmail.com>
2024-01-31 15:46:21 +0100

    form submission handled

--- 08-lorem-ipsum/src/App.jsx
+++ 08-lorem-ipsum/src/App.jsx
@@ -3,14 +3,19 @@ import data from "./data";
 
 const App = () => {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
   const handleInputChange = (e) => {
     setCount(e.target.value);
   };
-
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    const amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
   return (
     <section className="section-center">
       <h4>tired of boring lorem ipsum?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmitForm}>
         <label>paragraphs:</label>
         <input
           type="number"
@@ -22,10 +27,15 @@ const App = () => {
           value={count}
           onChange={handleInputChange}
         />
-        <button className="btn" type="submit" onSubmit={handleSubmitForm}>
+        <button className="btn" type="submit">
           generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item) => {
+          return <p>{item}</p>;
+        })}
+      </article>
     </section>
   );
 };
