250203751b82c7453d342f29259ee1957c260c55
mousta <moustaayman03@gmail.com>
2024-02-02 19:08:10 +0100

    hiding the submenu when not hovering on the corresponding navlink (a better approach)

--- 13-strapi-submenus/src/Submenu.jsx
+++ 13-strapi-submenus/src/Submenu.jsx
@@ -1,17 +1,23 @@
+import { useRef } from "react";
 import { useGlobalContext } from "./context";
 import sublinks from "./data";
 
 const Submenu = () => {
   const { pageId, setPageId } = useGlobalContext();
+  const submenuContainer = useRef(null);
 
   const currentPage = sublinks.find((item) => item.pageId === pageId);
-  const handleMouseLeave = () => {
-    setPageId(null);
+  const handleMouseLeave = (e) => {
+    const submenu = submenuContainer.current;
+    const { right, left, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = e;
+    if (clientX < left || clientX > right || clientY > bottom) setPageId(null);
   };
   return (
     <div
       className={currentPage ? "submenu show-submenu" : "submenu"}
       onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
     >
       <h5>{currentPage?.page}</h5>
       <div
