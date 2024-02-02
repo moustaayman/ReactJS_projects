f75382cd6cd3254ba9c7b6851906f4a4b475d85b
mousta <moustaayman03@gmail.com>
2024-02-02 18:32:12 +0100

    submenus logic implemented

--- 13-strapi-submenus/src/Submenu.jsx
+++ 13-strapi-submenus/src/Submenu.jsx
@@ -1,4 +1,31 @@
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
+
 const Submenu = () => {
-  return <div>Submenu</div>;
+  const { pageId } = useGlobalContext();
+
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  return (
+    <div className={currentPage ? "submenu show-submenu" : "submenu"}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, label, icon, url } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
