b53c6486b989906760fbe5c58ee4b01f1bcb6b5d
mousta <moustaayman03@gmail.com>
2024-02-02 08:19:02 +0100

    global context created & provider exported

---
+++ 12-sidebar/src/context.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setisSidebarOpen] = useState(false);
+  const [isModalOpen, setisModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setisSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setisSidebarOpen(false);
+  };
+  const openModal = () => {
+    setisModalOpen(true);
+  };
+  const closeModal = () => {
+    setisModalOpen(false);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isModalOpen,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+export default AppProvider;
