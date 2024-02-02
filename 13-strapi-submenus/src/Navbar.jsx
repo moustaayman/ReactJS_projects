1e05775e702ff8c87d7a7ff09415a99454bbb985
mousta <moustaayman03@gmail.com>
2024-02-02 16:05:30 +0100

    displayed the logo & sidebar button

--- 13-strapi-submenus/src/Navbar.jsx
+++ 13-strapi-submenus/src/Navbar.jsx
@@ -1,4 +1,19 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+import NavLinks from "./NavLinks";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        <NavLinks />
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
