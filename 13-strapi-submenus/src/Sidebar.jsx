b6d4770590857f32eea837acc13ff8e78147a8bb
mousta <moustaayman03@gmail.com>
2024-02-02 12:58:13 +0100

    rendered first part of the sidebar

--- 13-strapi-submenus/src/Sidebar.jsx
+++ 13-strapi-submenus/src/Sidebar.jsx
@@ -1,4 +1,17 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn">
+          <FaBars />
+        </button>
+      </div>
+    </nav>
+  );
 };
 export default Sidebar;
