9f66c57aa64d0aed31984e1c7501bf6d2f31c913
mousta <moustaayman03@gmail.com>
2024-02-02 12:57:47 +0100

    global context set

--- 13-strapi-submenus/src/context.jsx
+++ 13-strapi-submenus/src/context.jsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
 
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
