5fe6a4a51427cbeccc3a9826af3af483b48e8e4c
mousta <moustaayman03@gmail.com>
2024-02-02 08:53:23 +0100

    open sidebar and modal buttons displayed

--- 12-sidebar/src/Home.jsx
+++ 12-sidebar/src/Home.jsx
@@ -1,4 +1,17 @@
+import { useGlobalContext } from "./context";
+import { FaBars } from "react-icons/fa";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { openSidebar, openModal } = useGlobalContext();
+  return (
+    <main>
+      <button onClick={openSidebar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        show modal
+      </button>
+    </main>
+  );
 };
 export default Home;
