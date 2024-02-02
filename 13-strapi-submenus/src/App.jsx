e96723c1185b7b5b730832b2a27d5d8aa934fac4
mousta <moustaayman03@gmail.com>
2024-02-02 12:41:14 +0100

    Navbar, Hero, Sidebar, and Submenu are rendered

--- 13-strapi-submenus/src/App.jsx
+++ 13-strapi-submenus/src/App.jsx
@@ -1,4 +1,16 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
 };
 export default App;
