022eace9f998a5e62e42b11460ab57e2330c78e7
mousta <moustaayman03@gmail.com>
2024-02-05 09:07:04 +0100

    useReducer wa set

--- 14-cart/src/context.jsx
+++ 14-cart/src/context.jsx
@@ -1,18 +1,25 @@
+import { useReducer } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
+import { reducer } from "./reducer";
+import { CLEAR_CART } from "./actions";
+import cartItems from "./data";
 
 const AppContext = createContext();
 
 const defaultState = {
-  cart: new Map(),
+  cart: new Map(cartItems.map((item) => [item.id, item])),
   loading: false,
 };
 
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
   return (
-    <AppContext.Provider value={{ defaultState }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 export default AppProvider;
