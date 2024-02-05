a045a376f3f1f95c97ad4e452d510f1dfa275b8c
mousta <moustaayman03@gmail.com>
2024-02-05 08:09:30 +0100

    cart was set as a new Map()

--- 14-cart/src/context.jsx
+++ 14-cart/src/context.jsx
@@ -3,9 +3,18 @@ import { createContext } from "react";
 
 const AppContext = createContext();
 
-export const useGlobalContext = () => useContext(AppContext);
+const defaultState = {
+  cart: new Map(),
+  loading: false,
+};
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ defaultState }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 export default AppProvider;
+
+export const useGlobalContext = () => useContext(AppContext);
