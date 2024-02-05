af3706d3198ff28435080cd090e8bbbc27ac9c83
mousta <moustaayman03@gmail.com>
2024-02-05 13:19:52 +0100

    LOADING & DISPALY_CART variables exported

--- 14-cart/src/actions.js
+++ 14-cart/src/actions.js
@@ -2,3 +2,5 @@ export const CLEAR_CART = "CLEAR_CART";
 export const REMOVE_ITEM = "REMOVE_ITEM";
 export const INCREASE = "INCREASE";
 export const DECREASE = "DECREASE";
+export const LOADING = "LOADING";
+export const DISPLAY_CART = "DISPLAY_CART";
