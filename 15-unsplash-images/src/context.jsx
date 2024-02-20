b0a6cb4cef4bef853b673a41930a5f896571108b
mousta <moustaayman03@gmail.com>
2024-02-20 20:53:09 +0100

    dark theme logic set

--- 15-unsplash-images/src/context.jsx
+++ 15-unsplash-images/src/context.jsx
@@ -6,9 +6,16 @@ export const useGlobalContext = () => useContext(AppContext);
 
 const AppProvider = ({ children }) => {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
-  const toggleDarkTheme = () => setIsDarkTheme(!isDarkTheme);
+  const [searchText, setSearchText] = useState("cat");
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+  };
   return (
-    <AppContext.Provider value={{ isDarkTheme, toggleDarkTheme }}>
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, setSearchText }}
+    >
       {children}
     </AppContext.Provider>
   );
