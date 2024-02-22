54a7feebbca28c4d16faf5e77b2e5411b66e3af3
mousta <moustaayman03@gmail.com>
2024-02-22 18:15:38 +0100

    implemented the gallery fetching logic

--- 15-unsplash-images/src/Gallery.jsx
+++ 15-unsplash-images/src/Gallery.jsx
@@ -1,4 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useGlobalContext } from "./context";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=X3c-NbRfZvL-N_UKzp-VK9pNFp0pcvEdPdm8hEutyCg";
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { searchText } = useGlobalContext();
+  const response = useQuery({
+    queryKey: ["images", searchText],
+    queryFn: async () => {
+      const result = await axios.get(`${url}&query=${searchText}`);
+      return result.data;
+    },
+  });
+  console.log(response);
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+  console.log(results);
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No results found...</h4>
+      </section>
+    );
+  }
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={item.id}
+            alt={item.alt_description}
+            className="img"
+          ></img>
+        );
+      })}
+    </section>
+  );
 };
 export default Gallery;
