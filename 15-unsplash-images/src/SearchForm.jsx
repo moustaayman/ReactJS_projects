421739132c6fd086f82a7a777b6549522ad763cd
mousta <moustaayman03@gmail.com>
2024-02-22 18:17:01 +0100

    changing the search value accordingly to th value in the input

--- 15-unsplash-images/src/SearchForm.jsx
+++ 15-unsplash-images/src/SearchForm.jsx
@@ -1,14 +1,12 @@
 import { useGlobalContext } from "./context";
-const accessKey = "X3c-NbRfZvL-N_UKzp-VK9pNFp0pcvEdPdm8hEutyCg";
 
 const SearchForm = () => {
+  const { setSearchText } = useGlobalContext();
   const handleSubmit = (e) => {
-    // const { setSearchText } = useGlobalContext();
     e.preventDefault();
     const searchValue = e.target.elements.search.value;
-    // if (searchValue) setSearchText(searchValue);
     if (!searchValue) return;
-    console.log(searchValue);
+    setSearchText(searchValue);
   };
   return (
     <section>
