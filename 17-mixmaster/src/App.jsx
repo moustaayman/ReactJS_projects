b9e619c5b876b658a71e4fd415b7b3a1428e4a68
mousta <moustaayman03@gmail.com>
2024-07-06 14:44:53 +0100

    Components imported & rendered

--- 17-mixmaster/src/App.jsx
+++ 17-mixmaster/src/App.jsx
@@ -1,4 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Landing,
+  Cocktail,
+  HomeLayout,
+  Newsletter,
+  Error,
+  About,
+} from "./pages/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 export default App;
