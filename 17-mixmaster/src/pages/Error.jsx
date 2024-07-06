ae32a4b95e27abc97a5b692f836918ff6a1e5d70
mousta <moustaayman03@gmail.com>
2024-07-06 20:07:39 +0100

    added content

--- 17-mixmaster/src/pages/Error.jsx
+++ 17-mixmaster/src/pages/Error.jsx
@@ -1,4 +1,27 @@
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/not-found.svg";
+import { Link, useRouteError } from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  console.log(error);
+  if (error.status == 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to="/">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
--- 17-mixmaster/src/pages/HomeLayout.jsx
+++ 17-mixmaster/src/pages/HomeLayout.jsx
@@ -1,4 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 const HomeLayout = () => {
-  return <div>HomeLayout</div>;
+  return (
+    <div>
+      <Navbar />
+      <section className="page">
+        <Outlet />
+      </section>
+    </div>
+  );
 };
 export default HomeLayout;
