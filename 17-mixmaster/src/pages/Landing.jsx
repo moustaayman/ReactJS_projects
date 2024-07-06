754e97ffb0ede3c2497d9f939a7dd8b2c50c2743
mousta <moustaayman03@gmail.com>
2024-07-06 20:40:08 +0100

    set detching logic using useLoaderData

--- 17-mixmaster/src/pages/Landing.jsx
+++ 17-mixmaster/src/pages/Landing.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  const searchInput = "margarita";
+  const response = await axios.get(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`
+  );
+  return { drinks: response.data.drinks, searchInput };
+};
+
 const Landing = () => {
+  const { drinks, searchInput } = useLoaderData();
+  console.log(drinks);
   return <div>Landing</div>;
 };
+
 export default Landing;
