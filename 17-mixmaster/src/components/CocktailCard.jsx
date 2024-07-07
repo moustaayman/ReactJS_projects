c29096407cfd10f75866b0304e777789bc1e5d99
mousta <moustaayman03@gmail.com>
2024-07-07 15:29:19 +0100

    deleted some unused imports

--- 17-mixmaster/src/components/CocktailCard.jsx
+++ 17-mixmaster/src/components/CocktailCard.jsx
@@ -1,9 +1,7 @@
-import { Link, useOutletContext } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/CocktailCard";
 
 const CocktailCard = ({ image, name, id, info, glass }) => {
-  const data = useOutletContext();
-  // console.log(data);
   return (
     <Wrapper>
       <div className="img-container">
