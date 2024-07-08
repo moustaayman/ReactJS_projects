5fb51cda43289eccb7016d57bab3832c65e69e45
mousta <moustaayman03@gmail.com>
2024-07-08 22:07:14 +0100

    Newsletter form set

--- 17-mixmaster/src/pages/Newsletter.jsx
+++ 17-mixmaster/src/pages/Newsletter.jsx
@@ -1,4 +1,56 @@
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="Ayman"
+        />
+      </div>
+      {/* last name */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="Moustadrif"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        submit
+      </button>
+    </form>
+  );
 };
 export default Newsletter;
