35575d4c725935a4e73b056b0e5ec2f0a7e60678
mousta <moustaayman03@gmail.com>
2024-07-10 22:10:42 +0100

    set the search form

--- 17-mixmaster/src/components/SearchForm.jsx
+++ 17-mixmaster/src/components/SearchForm.jsx
@@ -1,4 +1,23 @@
-const SearchForm = () => {
-  return <div>SearchForm</div>;
+import { Form, useNavigation } from "react-router-dom";
+import Wrapper from "../assets/wrappers/SearchForm";
+
+const SearchForm = ({ SearchInput }) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+  return (
+    <Wrapper>
+      <Form className="form">
+        <input
+          type="search"
+          name="search"
+          className="form-input"
+          defaultValue={SearchInput}
+        />
+        <button type="submit" className="btn" disabled={isSubmitting}>
+          {isSubmitting ? "searching..." : "search"}
+        </button>
+      </Form>
+    </Wrapper>
+  );
 };
 export default SearchForm;
